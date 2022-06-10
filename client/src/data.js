a65b1e4ba93c88ab4e6860e870d8405f6436a1b6
breezybrow <browder.bree@gmail.com>
2022-06-10 01:04:59 +0000

    add two new models

--- client/src/data.js
+++ client/src/data.js
@@ -40,72 +40,72 @@
 
 
 export const sliderItems = [
-    {
-      id: 1,
-      img: "http://localhost:3000/img/abstract.png",
-      title: "ABSTRACT",
-      desc: "CHECK OUT OUR ABSTRACT MODELS.",
-      bg: "f5fafd",
-    },
-    {
-      id: 2,
-      img: "http://localhost:3000/img/crystal.png",
-      title: "CRYSTAL",
-      desc: "CHECK OUT OUR CRYSTAL MODELS.",
-      bg: "fcf1ed",
-    },
-    {
-      id: 3,
-      img: "http://localhost:3000/img/motorcycle.png",
-      title: "MOTORCYCLE",
-      desc: "CHECK OUT OUR MOTORCYCLE MODELS.",
-      bg: "fbf0f4",
-    },
-  ];
+  {
+    id: 1,
+    img: "http://localhost:3000/img/abstract.png",
+    title: "ABSTRACT",
+    desc: "CHECK OUT OUR ABSTRACT MODELS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "http://localhost:3000/img/crystal.png",
+    title: "CRYSTAL",
+    desc: "CHECK OUT OUR CRYSTAL MODELS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "http://localhost:3000/img/motorcycle.png",
+    title: "MOTORCYCLE",
+    desc: "CHECK OUT OUR MOTORCYCLE MODELS.",
+    bg: "fbf0f4",
+  },
+];
 
 
-  export const categories = [
-    {
-      id: 1,
-      img: "http://localhost:3000/img/abstract.png",
-      title: "ABSTRACT",
-    },
-    {
-      id: 2,
-      img: "http://localhost:3000/img/crystal.png",
-      title: "CRYSTAL",
-    },
-    {
-      id: 3,
-      img: "http://localhost:3000/img/motorcycle.png",
-      title: "MOTORCYCLE",
-    },
-  ];
+export const categories = [
+  {
+    id: 1,
+    img: "http://localhost:3000/img/abstract.png",
+    title: "ABSTRACT",
+  },
+  {
+    id: 2,
+    img: "http://localhost:3000/img/holbie-coin.png",
+    title: "COINS",
+  },
+  {
+    id: 3,
+    img: "http://localhost:3000/img/motorcycle.png",
+    title: "MOTORCYCLE",
+  },
+];
 
 
-  export const popularProducts = [
-    {
-      id:1,
-      img:"http://localhost:3000/img/abstract.png",
-    },
-    {
-      id:2,
-      img:"http://localhost:3000/img/crystal.png",
-    },
-    {
-      id:3,
-      img:"http://localhost:3000/img/motorcycle.png",
-    },
-    {
-      id:4,
-      img:"http://localhost:3000/img/abstract.png",
-    },
-    {
-      id:5,
-      img:"http://localhost:3000/img/crystal.png",
-    },
-    {
-      id:6,
-      img:"http://localhost:3000/img/motorcycle.png",
-    },
-  ]
+export const popularProducts = [
+  {
+    id:1,
+    img:"http://localhost:3000/img/abstract.png",
+  },
+  {
+    id:2,
+    img:"http://localhost:3000/img/crystal.png",
+  },
+  {
+    id:3,
+    img:"http://localhost:3000/img/motorcycle.png",
+  },
+  {
+    id:4,
+    img:"http://localhost:3000/img/glow-cube.png",
+  },
+  {
+    id:5,
+    img:"http://localhost:3000/img/holbie-coin.png",
+  },
+  {
+    id:6,
+    img:"http://localhost:3000/img/motorcycle.png",
+  },
+]
